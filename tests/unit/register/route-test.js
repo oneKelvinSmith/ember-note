9ed939e9b6eba0e0d73d68d525ea79f40ff7646c
Kelvin Smith <oneKelvinSmith@gmail.com>
2015-12-08 14:24:40 +0200

    Add more elsint rules

--- tests/unit/register/route-test.js
+++ tests/unit/register/route-test.js
@@ -1,4 +1,4 @@
-import {moduleFor, test} from 'ember-qunit';
+import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:register', 'Unit | Route | register', {
   // Specify the other units that are required for this test.
