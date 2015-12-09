283034fa6b10db54c943f5711240c759b6b2b027
Kelvin Smith <oneKelvinSmith@gmail.com>
2015-12-09 23:19:41 +0200

    Add route and template for registering new users

--- app/register/route.js
+++ app/register/route.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    addNew: function() {
+      const user = this.store.createRecord('user', {
+        name: this.controller.get('name')
+      });
+
+      user
+        .save()
+        .then(() => {
+          console.log('save successful');
+          const name = this.controller.get('name');
+          const message = `A new user with name "${name}" was added!`;
+
+          this.controller.set('message', message);
+        }, () => console.log('save failed'));
+    }
+  }
 });
